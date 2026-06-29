f8518cc2a5694d0647ce9ccec1b20dc2212b5571
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-29 22:26:33 +0400

    added routes/users.js logic

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+import {Router} from 'express';
+
+import controller from '../controllers/users.js';
+import schema from '../middlewares/schemas/users.schema.js';
+import validation from '../middlewares/validation.js';
+
+const router = new Router();
+
+router.post(
+  '/register',
+  validation(schema.register, 'body'),
+  controller.register,
+);
+
+router.post(
+  '/login',
+  validation(schema.login, 'body'),
+  controller.login,
+);
+
+router.post(
+ '/activate',
+ validation(schema.activate, 'body'),
+ controller.activate,
+);
+
+router.post(
+  '/logout',
+  controller.logout,
+);
+
+router.get(
+  '/profile',
+  controller.profile,
+);
+
+export default router;

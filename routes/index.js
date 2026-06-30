f516187a88813c4fcbabd9aa03374f20abba04f0
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-30 11:31:59 +0400

    updated routes/index.js logic

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ import {Router} from 'express';
 
 import usersRouter from './users.js';
 import postsRouter from './posts.js';
+import chatRouter from './chat.js';
 
 const router = new Router();
 
 router.use('/users', usersRouter);
 router.use('/posts', postsRouter);
+router.use('/api/chat', chatRouter);
 
 export default router;

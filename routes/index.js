795f7a760f0a7818d45cc6e34f524dfdae56021b
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-06-30 12:01:20 +0400

    updated routes/index.js logic

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ import chatRouter from './chat.js';
 
 const router = new Router();
 
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
 router.use('/users', usersRouter);
 router.use('/posts', postsRouter);
 router.use('/api/chat', chatRouter);
